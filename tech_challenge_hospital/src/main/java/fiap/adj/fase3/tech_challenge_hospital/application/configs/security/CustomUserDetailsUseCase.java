76b82e0cf51a06fd42b38cd168db67fe6181da3b
juniorsmartins <junior.soares.martins@proton.me>
2025-09-17 17:22:48 -0300

    REFACTOR: ajuste de melhoria.

--- tech_challenge_hospital/src/main/java/fiap/adj/fase3/tech_challenge_hospital/application/configs/security/CustomUserDetailsUseCase.java
+++ tech_challenge_hospital/src/main/java/fiap/adj/fase3/tech_challenge_hospital/application/configs/security/CustomUserDetailsUseCase.java
@@ -2,6 +2,7 @@ package fiap.adj.fase3.tech_challenge_hospital.application.configs.security;
 
 import fiap.adj.fase3.tech_challenge_hospital.infrastructure.daos.UserDao;
 import fiap.adj.fase3.tech_challenge_hospital.infrastructure.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,20 +10,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsUseCase implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsUseCase(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        UserDao userDao = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        UserDao userDao = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
 
-        var authorities = userDao.getRoles().stream()
+        var authorities = userDao.getRoles()
+                .stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .toList();
 
